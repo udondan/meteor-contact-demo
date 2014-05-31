3d4488ff34e188385ee291aa7c7c39521e84bf7c
udondan <deemes79@gmail.com>
2014-05-31 12:14:54 +0200

    Added 404 support
    because why not

--- routes.js
+++ routes.js
@@ -1,7 +1,14 @@
-if(Meteor.isClient) {
-  Router.map(function() {
-    this.route("home", {
-      path: "/"
-    });
+Router.map(function() {
+  this.route("home", {
+    path: "/"
   });
-}
+  this.route("notFound", {
+    path: '*',
+    where: 'server',
+    action: function() {
+      this.response.statusCode = 404;
+      this.response.end(Handlebars.templates['404']());
+    }
+  });
+});
+
